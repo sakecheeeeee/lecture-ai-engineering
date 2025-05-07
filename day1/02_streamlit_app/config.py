044f7b0b725ca1f983ab3ae80c940bcd2837b2e8
sakecheeeeee <sakecheeeeee@gmail.com>
2025-05-07 10:11:30 +0900

    gemmaからdeepseekを使用するように変更

--- day1/02_streamlit_app/config.py
+++ day1/02_streamlit_app/config.py
@@ -1,3 +1,5 @@
 # config.py
 DB_FILE = "chat_feedback.db"
-MODEL_NAME = "google/gemma-2-2b-jpn-it"
+MODEL_NAME = "deepseek-ai/DeepSeek-Prover-V2-7B" #モデルをgemmaからdeepseekに変更
+
+#MODEL_NAME = "google/gemma-2-2b-jpn-it"
